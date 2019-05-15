d10ebe1ea447e16e0646390807beeb12101033c9
josue <josue.tamayo@cbtis72.edu.mx>
2019-05-15 16:46:36 -0500

    3.Agregando Fragmentos(pag 23)

--- app/src/main/java/com/example/floatingactionbotton/MainActivity.java
+++ app/src/main/java/com/example/floatingactionbotton/MainActivity.java
@@ -32,34 +32,50 @@ public class MainActivity extends AppCompatActivity /*implements View.OnClickLis
         setContentView(R.layout.activity_main);
          toolbar = (Toolbar) findViewById(R.id.toolbar);
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+       BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectecLister);
         toolbar.setTitle("Shop");
         toolbar.setTitleTextColor(Color.WHITE);
+        loadFragment(new StoreFragment());
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectecLister = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-           // Fragment fragment;
+            Fragment fragment;
             switch (item.getItemId()) {
                 case R.id.navigation_shop:
                     toolbar.setTitle("Shop");
+                    fragment = new StoreFragment();
+                    loadFragment(fragment);
                     return true;
                 case R.id.navigation_gifts:
                     toolbar.setTitle("My Gifts");
+                    fragment = new GiftsFragment();
+                    loadFragment(fragment);
                     return true;
                 case R.id.navigation_cart:
                     toolbar.setTitle("Cart");
+                    fragment = new CartFragment();
+                    loadFragment(fragment);
                     return true;
                 case R.id.navigation_porfile:
                     toolbar.setTitle("Profile");
+                    fragment = new ProfileFragment();
+                    loadFragment(fragment);
                     return true;
             }
             return false;
         }
     };
+    private void loadFragment(Fragment fragment){
+        //load fragment
+        FragmentTransaction transaction=getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container,fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 }
 
     //setSupportActionBar(toolbar);
