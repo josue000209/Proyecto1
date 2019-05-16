a912552d989dcb8776a862a4de02b40419968830
josue <josue.tamayo@cbtis72.edu.mx>
2019-05-15 20:28:14 -0500

    4.Implementación de ShopFragment-Visualizacion de elementos en la cuadrícula (pag 25)

--- app/src/main/java/com/example/floatingactionbotton/StoreFragment.java
+++ app/src/main/java/com/example/floatingactionbotton/StoreFragment.java
@@ -2,38 +2,180 @@ package com.example.floatingactionbotton;
 
 import android.content.Context;
 import android.net.Uri;
+import android.content.res.Resources;
+import android.content.Context;
+import android.graphics.Rect;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.util.Log;
+import android.util.TypedValue;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.json.JSONArray;
+import java.util.ArrayList;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 
 public class StoreFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static  final String TAG = StoreFragment.class.getSimpleName();
+    private static final String URL= "http://puntosingular.mx/json/movies_2017.json";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RecyclerView recyclerView;
+    private List<Movie> movieList;
+    private StoreAdapter mAdapter;
+
+    public StoreFragment(){
+
+    }
+        public static StoreFragment newInstance(String param1, String param2) {
+            StoreFragment fragment = new StoreFragment();
+            Bundle args = new Bundle();
+            fragment.setArguments(args);
+            return fragment;
+        }
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+        @Override
+        public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState){
+            View view =inflater.inflate(R.layout.fragment_store,container,false);
 
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            recyclerView=view.findViewById(R.id.recycler_View);
+            movieList = new ArrayList<>();
+            mAdapter = new StoreAdapter(getActivity(),movieList);
+
+            RecyclerView.LayoutManager mLayoutManager= new GridLayoutManager(getActivity(),3);
+            recyclerView.setLayoutManager(mLayoutManager);
+            recyclerView.addItemDecoration(new GridSpacingItemDecoration(2,dpToPx(8),true));
+            recyclerView.setItemAnimator(new DefaultItemAnimator());
+            recyclerView.setAdapter(mAdapter);
+            recyclerView.setNestedScrollingEnabled(false);
+
+            fetchStoreItems();
+
+            return view;
+        }
+
+        private void fetchStoreItems(){
+        JsonArrayRequest request= new JsonArrayRequest(URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                if (response == null) {
+                    Toast.makeText(getActivity(), "Couldn't fetch the store items! Please try again.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                List<Movie> items = new
+                        Gson().fromJson(response.toString(), new TypeToken<List<Movie>>() {
+                }.getType());
+                movieList.clear();
+                movieList.addAll(items);
+
+                mAdapter.notifyDataSetChanged();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG,"ERROR: "+error.getMessage());
+                Toast.makeText(getActivity(),"ERROR "+ error.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        MyApplication.getInstance().addToRequestQueue(request);
+        }
+        public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration
+        {
+            private int spanCount;
+            private int spacing;
+            private  boolean includeEdge;
+            public GridSpacingItemDecoration(int spanCount,int spacing,boolean includeEdge){
+                this.spacing=spacing;
+                this.spanCount=spanCount;
+                this.includeEdge=includeEdge;
+            }
+
+            @Override
+            public void getItemOffsets(Rect outRect, View view, RecyclerView parent,RecyclerView.State state){
+                int position =parent.getChildAdapterPosition(view);
+                int column=position % spanCount;
+                if (includeEdge){
+                    outRect.left = spacing-column*spacing/spanCount;//item   spacing-column*((1f/spanCount)*spacing)
+
+                            outRect.right =(column+1)*spacing/spanCount;// (column + 1)*((1f/spanCount)*spacing)
+
+                  if(position<spanCount){
+                      outRect.top=spacing;
+                  }
+                  outRect.bottom=spacing;
+                }else{
+                    outRect.left=column*spacing/spanCount;
+                    outRect.right=spacing-(column+1)*spacing/spanCount;
+                    if(position>=spanCount){
+                        outRect.top=spacing;
+                    }
+                }
+            }
+        }
+        private int dpToPx(int dp){
+        Resources r= getResources();
+        return
+                Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,dp,r.getDisplayMetrics()));
+        }
+        class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.MyViewHolder>
+        {
+            private Context context;
+            private List<Movie> movieList;
+
+            public class MyViewHolder extends RecyclerView.ViewHolder{
+                public TextView name,price;
+                public ImageView thumbnail;
+
+                public MyViewHolder(View view){
+                    super(view);
+                    name=view.findViewById(R.id.title);
+                    price=view.findViewById(R.id.price);
+                    thumbnail=view.findViewById(R.id.thumbnail);
+                }
+            }
+            public StoreAdapter(Context context, List<Movie> movieList){
+                this.context=context;
+                this.movieList=movieList;
+            }
+            @Override
+            public MyViewHolder onCreateViewHolder (ViewGroup parent, int viewType){
+                View itemView =LayoutInflater.from(parent.getContext()).inflate(R.layout.store_item_row,parent,false);
+                return new MyViewHolder(itemView);
+            }
+            @Override
+            public void onBindViewHolder (MyViewHolder holder, final int position)
+            {
+                final Movie movie =movieList.get(position);
+                holder.name.setText(movie.getTitle());
+                holder.price.setText(movie.getPrice());
+
+                Glide.with(context)
+                        .load(movie.getImage())
+                        .into(holder.thumbnail);
+            }
+            @Override
+            public int getItemCount (){
+                return movieList.size();
+            }
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_store, container, false);
-    }
 
-}
